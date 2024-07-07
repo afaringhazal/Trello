5bd9975b70058b73cdb8e2d60ed91052d6a36a89
gazelle <Afaringhazal@gmail.com>
2024-07-07 20:21:43 +0330

    fix

--- client/app/web-socket/webSocketComp.tsx
+++ client/app/web-socket/webSocketComp.tsx
@@ -1,8 +1,5 @@
 
-
-
-
-
+import {useEffect} from 'react'
 function WebSocketComponent() {
     useEffect(() => {
     // WebSocket connection setup
@@ -10,16 +7,16 @@ function WebSocketComponent() {
     // Event listeners
     socket.onopen = function (event) {
     console.log('WebSocket connection established');
-    });
+    };
     socket.onmessage = function (event) {
     console.log('Message from server:', event.data);
-    });
+    };
     socket.onerror = function (error) {
     console.error('WebSocket error:', error);
-    });
+    };
     socket.onclose = function (event) {
     console.log('WebSocket connection closed:', event);
-    });
+    };
     // Cleanup function
     return () => {
     socket.close();
